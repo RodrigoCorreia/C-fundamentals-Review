07aafc8863800ee4cd531b4f8fbce2a19272381a
Rodrigo Correia <rodrigocorrei@gmail.com>
2018-10-22 22:06:55 -0300

    Add a class return method

--- CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
+++ CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -13,17 +13,51 @@ namespace CursoCSharp.ClassesEMetodos
 
 		public int Somar(int a, int b, int c)
 		{
-			return Somar(a,b) + c;
+			return Somar(a, b) + c;
 		}
 
-		public int Subtrair (int a,int b)
+		public int Subtrair(int a, int b)
 		{
 			return a - b;
 		}
 
-		public int Multiplicar (int a, int b)
+		public int Multiplicar(int a, int b)
 		{
 			return a * b;
+		}
+	}
+
+	class CalculadoraCadeia
+	{
+		int memoria;
+
+		public CalculadoraCadeia Somar(int a)
+		{
+			memoria += a;
+			return this;
+		}
+
+		public CalculadoraCadeia Multiplicar (int a)
+		{
+			memoria *= a;
+			return this;
+		}
+
+		public CalculadoraCadeia Limpar()
+		{
+			memoria = 0;
+			return this;
+		}
+
+		public CalculadoraCadeia Imprimir()
+		{
+			Console.WriteLine(memoria);
+			return this;
+		}
+
+		public int Resultado()
+		{
+			return memoria;
 		}
 	}
 
@@ -39,6 +73,16 @@ namespace CursoCSharp.ClassesEMetodos
 			Console.WriteLine(Calculadora.Subtrair(2,7));
 
 			Console.WriteLine(Calculadora.Multiplicar(4,4));
+
+			//////////////////////////////////////////////////
+			
+			var CalculadoraCadeia = new CalculadoraCadeia();
+
+			//É possivel encadear pois o retorno dos metodos é o proprio objeto.
+			CalculadoraCadeia.Somar(3).Multiplicar(3).Imprimir().Limpar().Imprimir();
+
+			resultado = CalculadoraCadeia.Somar(3).Multiplicar(2).Resultado();
+			Console.WriteLine(resultado);
 		}
 	}
 }

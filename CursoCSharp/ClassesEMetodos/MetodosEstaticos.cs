6c88785ff2c661c60afa4f1a06f16c69fc56a848
Rodrigo Correia <rodrigocorrei@gmail.com>
2018-12-10 22:14:10 -0200

    Add static class

---
+++ CursoCSharp/ClassesEMetodos/MetodosEstaticos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+	public class CalculadoraEstatica
+	{
+		//Metodo de Classe ou Método estático
+		public static int Multiplicar (int a, int b)
+		{
+			return a * b;
+		}
+
+		//Metodo de Intência
+		public int Somar(int a, int b)
+		{
+			return a + b;
+		}
+	}
+
+	class MetodosEstaticos
+	{
+		public static void Executar()
+		{
+			var resultado = CalculadoraEstatica.Multiplicar(2, 2);
+			Console.WriteLine("Resultado: {0}", resultado);
+
+			Console.WriteLine(CalculadoraEstatica.Multiplicar(2, 2));
+
+			CalculadoraEstatica Calc = new CalculadoraEstatica();
+
+			Console.WriteLine(Calc.Somar(2,2));
+			
+			//Dá erro
+			// Console.WriteLine(Calc.Multiplicar(2, 2));
+		}
+	}
+}
